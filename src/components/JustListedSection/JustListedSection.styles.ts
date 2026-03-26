6091790bc63e1c0857c2c65d63e07f8c786483d9
ZGirouard <zgirouard2@huskers.unl.edu>
2026-03-25 20:25:14 -0500

    Just listed

--- src/components/JustListedSection/JustListedSection.styles.ts
+++
@@ -1,9 +0,0 @@
-import styled from '@emotion/styled'
-
-export const Row = styled.div`
-  display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-  gap: 16px;
-  align-items: flex-start;
-`
--- src/components/JustListedSection/JustListedSection.tsx
+++ src/components/JustListedSection/JustListedSection.tsx
@@ -1,37 +1,43 @@
 import { useMemo } from 'react'
+import type { ProductListing } from '../../data/products'
 import { products } from '../../data/products'
-import SmallProductCard from '../SmallProductCard/SmallProductCard'
-import { Section, Title } from '../MostWatchedElectricGuitarsSection/MostWatchedElectricGuitarsSection.styles'
-import { Row } from './JustListedSection.styles'
+import Product from '../Product/Product'
+import { Title } from '../TopBrandsSection/TopBrandsSection.styles'
+import { Grid, Section } from '../RecentlyViewedSection/RecentlyViewedSection.styles'
 
-function shuffle<T>(items: T[]): T[] {
-  const copy = [...items]
+function pickRandomProducts(source: ProductListing[], count: number): ProductListing[] {
+  const copy = [...source]
   for (let i = copy.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1))
     ;[copy[i], copy[j]] = [copy[j], copy[i]]
   }
-  return copy
+  return copy.slice(0, Math.min(count, copy.length))
 }
 
 const PICK_COUNT = 6
 
 export default function JustListedSection() {
-  const items = useMemo(() => shuffle(products).slice(0, PICK_COUNT), [])
+  const items = useMemo(() => pickRandomProducts(products, PICK_COUNT), [])
 
   return (
     <Section aria-labelledby="just-listed-heading">
       <Title id="just-listed-heading">Just listed</Title>
-      <Row>
+      <Grid>
         {items.map((item) => (
-          <SmallProductCard
+          <Product
             key={item.id}
             imageSrc={item.imageSrc}
             imageAlt={item.imageAlt}
+            description={item.description}
+            quality={item.quality}
             price={item.price}
-            name={item.description}
+            reverbBump={item.reverbBump}
+            showFreeShipping={item.showFreeShipping}
+            showReturnPolicy={item.showReturnPolicy}
+            showGreatValue={item.showGreatValue}
           />
         ))}
-      </Row>
+      </Grid>
     </Section>
   )
 }
